6e06f298da531970e22201edfb7e3dd89b05e66d
sagamalhotra22 <malhotra.sag@northeastern.edu>
2024-11-30 19:03:57 -0500

    Created the root cli command using cobra

---
+++ cmd/root.cmd
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"log"
+)
+
+// It is the base command which called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "my_iac_tool",
+	Short: "A simple tool to create and desctroy GCP compute engine",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func init() {
+	// Subcommands in root command
+    // Add command created the infrastructure
+	rootCmd.AddCommand(applyCmd)
+    // Destroys the infrastructure
+	rootCmd.AddCommand(destroyCmd)
+}
